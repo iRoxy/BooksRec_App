4e448aceddcac98ebfdccdd8197190fddbca7c43
iroxy <raquel_blackrose@hotmail.com>
2015-12-10 05:30:57 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209191544) do
+ActiveRecord::Schema.define(version: 20151210052345) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20151209191544) do
     t.string   "author"
     t.string   "genre"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
